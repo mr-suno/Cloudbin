4b4fd951184e5335d683f368da65c59be60994a6
Suno <ilovesuno@proton.me>
2024-07-20 22:13:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const PORT = 8000
 
 
 app.get("/", (req, res) => {
-   res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.post("/", (req, res) => {
    res.send("loadstring(game:HttpGet('https://github.com/mr-suno/Starry/releases/latest/download/main.lua'))()")
 });
+
+// app.post("/", (req, res) => {
+//    res.send("loadstring(game:HttpGet('https://github.com/mr-suno/Starry/releases/latest/download/main.lua'))()")
+// });
 
 
 app.listen(PORT, () => { console.log(`Webpage loaded on :${PORT}`) });
