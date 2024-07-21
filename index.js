b5761252559b44fea2b43f7e73882738f2528c6e
Suno <ilovesuno@proton.me>
2024-07-20 21:25:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const app = express()
+const PORT = 8080
+
+
+app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/", (req, res) => {
+   res.send("loadstring(game:HttpGet('https://github.com/mr-suno/Starry/releases/latest/download/main.lua'))()")
+});
+
+
+app.listen(PORT, () => { console.log("Webpage loaded on :8080") });
