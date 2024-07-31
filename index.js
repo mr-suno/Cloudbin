c9748219f746549a7bd630418264828efa583b77
Suno <ilovesuno@proton.me>
2024-07-31 00:00:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const PORT = 8000
 
 
 app.get("/", (req, res) => {
-   res.send("loadstring(game:HttpGet('https://github.com/mr-suno/Starry/releases/latest/download/main.lua'))()")
+   res.send("loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/089e8f5b8ac259500a44ccd0537ab253.lua'))()")
 });
-
-// app.post("/", (req, res) => {
-//    res.send("loadstring(game:HttpGet('https://github.com/mr-suno/Starry/releases/latest/download/main.lua'))()")
-// });
-
 
 app.listen(PORT, () => { console.log(`Webpage loaded on :${PORT}`) });
